961c9a2ec859b687e81486e3e9d833c64d316d2a
Sureshj7 <sureshj2625@gmail.com>
2026-02-27 14:33:35 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,59 @@
+const btn = document.querySelector("button");
+const formInfo = document.getElementById("form-info");
+const msg = document.getElementById("form-msg");
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const formData = new FormData(form);
+
+  //   console.log(Object.fromEntries(formData.entries())); //to get formData as an object
+  //   console.log(formData.get("username")); //to get formData as an indivually
+
+  const username = formData.get("username");
+  const email = formData.get("email");
+  const password = formData.get("password");
+  const ConfirmPassword = formData.get("ConfirmPassword");
+
+  const userNameInput = document.getElementById("username");
+  const emailInput = document.getElementById("email");
+  const passwordInput = document.getElementById("password");
+  const confirmPasswordInput = document.getElementById("ConfirmPassword");
+
+  function showError(input, message) {
+    input.style.outlineColor = "red";
+    input.focus();
+    formInfo.style.backgroundColor = "red";
+    msg.textContent = message;
+    msg.style.color = "white";
+  }
+  function showSuccess() {
+    formInfo.style.backgroundColor = "Green";
+    msg.textContent = "Thank you for Signed";
+    msg.style.color = "Black";
+    console.log(Object.fromEntries(formData.entries()));
+
+    form.reset();
+  }
+
+  if (username === "") {
+    showError(userNameInput, "Enter UserName");
+    return;
+  }
+  if (!email === "" || !email.includes("@gmail.com")) {
+    showError(emailInput, "Enter Valid Email");
+    return;
+  }
+  if (password === "" && ConfirmPassword === "") {
+    showError(passwordInput, "Enter a Password");
+    return;
+  }
+
+  if (password !== ConfirmPassword) {
+    showError(passwordInput, "password Doesn't match");
+    return;
+  }
+
+  console.log(Object.fromEntries(formData.entries()));
+  showSuccess();
+});
